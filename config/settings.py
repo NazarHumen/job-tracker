512908efddbb0defa9139634e341d7fb64051692
NazarHumen <nazargumen172@gmail.com>
2026-04-28 13:41:35 +0300

    chore(config): register jobs app and configure postgres, media, auth

--- config/settings.py
+++ config/settings.py
@@ -121,3 +121,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+LOGIN_REDIRECT_URL = "dashboard"
+LOGOUT_REDIRECT_URL = "home"
+LOGIN_URL = "login"
