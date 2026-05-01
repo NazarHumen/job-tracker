5c3d3371f77eeae9cace8206ba588aa4119889b4
NazarHumen <nazargumen172@gmail.com>
2026-05-01 12:13:49 +0300

    feat(auth): wire LoginForm into /accounts/login/

--- config/urls.py
+++ config/urls.py
@@ -15,12 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from jobs.forms import LoginForm
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        'accounts/login/',
+        LoginView.as_view(authentication_form=LoginForm),
+        name='login',
+    ),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('jobs.urls')),
 ]
